e49f49e54d18fa2ef322119083ec75e56a42e022
yang3666 <2293381927@qq.com>
2023-05-15 17:29:34 +0800

    spring——Spring 注入集合——在集合中设置普通类型的值

--- src/main/java/com/jy/MainApp.java
+++ src/main/java/com/jy/MainApp.java
@@ -1,26 +1,25 @@
 package com.jy;
 
-import org.springframework.beans.factory.BeanFactory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class MainApp {
 
-    public static void main(String[] args) {
-        /**
-         * 使用BeanFactory获取Helloworld对象
-         */
-//        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-//        HelloWorld obj = context.getBean("helloWorld",HelloWorld.class);
-//        obj.getMessage();
+    private static final Log LOGGER = LogFactory.getLog(MainApp.class);
 
-        /**
-         * 使用 FileSystemXmlApplicationContext 加载指定路径下的配置文件 Bean.xml
-         */
-        BeanFactory context = new FileSystemXmlApplicationContext("D:\\eclipe workspace\\spring workspace\\HelloSpring\\src\\Beans.xml");
-        HelloWorld obj = context.getBean("helloWorld", HelloWorld.class);
-        obj.getMessage();
+    public static void main(String[] args) {
+
+        //获取 ApplicationContext 容器
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        JavaCollection javaCollection = context.getBean("javaCollection", JavaCollection.class);
+
+        LOGGER.info(javaCollection.toString());
+
+        System.out.println(javaCollection);
+
     }
 
 }
