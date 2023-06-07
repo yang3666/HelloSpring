de73ae6307fd731ae5c09da28eff551c4fe8c757
yang3666 <2293381927@qq.com>
2023-06-08 00:28:50 +0800

    spring自动装配

--- src/main/java/com/jy/MainApp.java
+++ src/main/java/com/jy/MainApp.java
@@ -1,26 +1,14 @@
 package com.jy;
 
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class MainApp {
 
     public static void main(String[] args) {
-        /**
-         * 使用BeanFactory获取Helloworld对象
-         */
-//        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-//        HelloWorld obj = context.getBean("helloWorld",HelloWorld.class);
-//        obj.getMessage();
-
-        /**
-         * 使用 FileSystemXmlApplicationContext 加载指定路径下的配置文件 Bean.xml
-         */
-        BeanFactory context = new FileSystemXmlApplicationContext("D:\\eclipe workspace\\spring workspace\\HelloSpring\\src\\Beans.xml");
-        HelloWorld obj = context.getBean("helloWorld", HelloWorld.class);
-        obj.getMessage();
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        Employee employee = context.getBean("employee", Employee.class);
+        System.out.println(employee);
     }
 
 }
